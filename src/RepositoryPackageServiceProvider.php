64fbe1886104eab119e018c8aea458c86a82757d
Kolawole Ezekiel <doneazy911@gmail.com>
2020-07-15 18:07:29 +0100

    initial commit

---
+++ src/RepositoryPackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Eazybright\RepositoryPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryPackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // 
+    }
+
+    public function boot()
+    {
+        // 
+    }
+}
