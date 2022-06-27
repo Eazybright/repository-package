2d00d8bead44c6e1e93c5f14237a6fe2a6a862cf
Kolawole Ezekiel <ezekiel.kolawole@cudium.com>
2022-06-27 20:12:00 +0100

    support php 8

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,6 +20,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        // perform environment setup
+      // perform environment setup
     }
 }
